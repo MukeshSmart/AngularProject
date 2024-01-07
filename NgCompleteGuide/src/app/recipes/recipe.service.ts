2b81edaa1793dda1d7f1a8bb7ae260fc80e3593a
Mukesh Dubey <mukesh14mca60@gmail.com>
2024-01-07 17:37:25 +0530

    Cross component communication using recipe service

--- NgCompleteGuide/src/app/recipes/recipe.service.ts
+++ NgCompleteGuide/src/app/recipes/recipe.service.ts
@@ -1,14 +1,15 @@
-import { Injectable } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 import { Recipe } from "./recipe.model";
 
 @Injectable()
-export class RecipeService{
-    recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-        new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
-      ];
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+  ];
 
-      getRecipes(){
-        return this.recipes.slice();
-      }
+  getRecipes() {
+    return this.recipes.slice();
+  }
 }
