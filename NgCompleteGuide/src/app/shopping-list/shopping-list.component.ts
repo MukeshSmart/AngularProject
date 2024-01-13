60dcb1c938f25c869aa663524942a11d8d5a5488
Mukesh Dubey <mukesh14mca60@gmail.com>
2024-01-13 21:47:03 +0530

    Replaced EventEmiiter with Subject

--- NgCompleteGuide/src/app/shopping-list/shopping-list.component.ts
+++ NgCompleteGuide/src/app/shopping-list/shopping-list.component.ts
@@ -1,20 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
- ingredients : Ingredient[];
-  constructor(private shoppingListService : ShoppingListService) { }
-
+export class ShoppingListComponent implements OnInit, OnDestroy {
+  ingredients: Ingredient[];
+  private igChangedSub: Subscription;
+  constructor(private shoppingListService: ShoppingListService) { }
+  
   ngOnInit() {
-   this.ingredients = this.shoppingListService.getIngredients();
-   this.shoppingListService.ingredientsChanged
-   .subscribe((ingredients : Ingredient[])=>this.ingredients = ingredients)
+    this.ingredients = this.shoppingListService.getIngredients();
+    this.igChangedSub = this.shoppingListService.ingredientsChanged
+      .subscribe((ingredients: Ingredient[]) => this.ingredients = ingredients)
+  }
+  ngOnDestroy(): void {
+    this.igChangedSub.unsubscribe();
   }
 }
